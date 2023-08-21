43a6b86abb4ab89eedfba14755907e13e249003d
MostakemHossain <mostakememon123@gmail.com>
2023-08-21 21:15:23 +0600

    websites-layout created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,52 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+const App = () => (
+  <main className="relative">
+
+    Nav
+    <section className="xl:padding-1 wide:padding-r  padding-b">
+      Hero
+    </section>
+
+    <section className="padding">
+      popular products
+
+    </section>
+
+    <section className="padding">
+      superQuality
+
+    </section>
+
+    <section className="padding-x py-10">
+      services
+
+    </section>
+
+    <section className="padding">
+      special offer
+
+    </section>
+
+
+
+    <section className="bg-pale-blue padding">
+      customerReviews
+
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      Subscribe
+
+    </section>
+    <section className="padding-x padding-t bg-black pb-8 ">
+      Footer
+
+    </section>
+
+
+
+
+
+  </main>
+
+);
+
+export default App;
